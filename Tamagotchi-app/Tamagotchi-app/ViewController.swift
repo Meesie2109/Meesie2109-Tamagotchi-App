4e7a7ff45daee50673d75a0059cef50696d7d7f3
Meesie2109 <meesrootjes@icloud.com>
2022-03-09 22:36:56 +0100

    Added  a function to count touches and a timer
    Added a function/gesture recognizer to check if a user taps or swipes the screen. And also added a timer that counts down from 10,

--- Tamagotchi-app/Tamagotchi-app/ViewController.swift
+++ Tamagotchi-app/Tamagotchi-app/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    var timer = Timer()
     var character = Character.init(name: "", gender: "", health: 100, food: 100, attention: 100, status: "")
 
     override func viewDidLoad() {
@@ -28,11 +28,13 @@ class ViewController: UIViewController {
             frame: CGRect(
                 x: 0,
                 y: 0,
-                width: 100,
-                height: 100
+                width: 150,
+                height: 150
             )
         )
         characterView.center = view.center
+        
+        GestureRecognizer(viewInstance: characterView)
         
         //Calls the timerSubView from a viewFile
         let timerView = TimerView(
@@ -44,13 +46,57 @@ class ViewController: UIViewController {
             )
         )
         
+        countDown()
+        
         //Adds the subviews to the main view
         view.addSubview(characterView)
         view.addSubview(timerView)
     }
     
+    //MARK: - Gesture Recognizer
     
+    //A function to tell if the user taps on the screen
+    private func GestureRecognizer(viewInstance: UIView) {
+        
+        let gestureRecognizer = UITapGestureRecognizer(
+            target: self, action: #selector(gestureFired(_:)))
 
-
+        //Amount of taps required
+        gestureRecognizer.numberOfTapsRequired = 1
+        gestureRecognizer.numberOfTouchesRequired = 1
+        
+//        let gestureRecognizer =  UISwipeGestureRecognizer(target: self, action:
+//        #selector(gestureFired(_:)))
+//        gestureRecognizer.direction = .right
+//        gestureRecognizer.numberOfTouchesRequired = 1
+        
+        //Adds the GestureRecognizer to the view
+        viewInstance.addGestureRecognizer(gestureRecognizer)
+        viewInstance.isUserInteractionEnabled = true
+    }
+    
+    @objc private func gestureFired(_ gesture: UITapGestureRecognizer){
+        if let firedView = gesture.view {
+            firedView.backgroundColor = .black
+        }
+    }
+    
+    //MARK: - Count Down / Timer
+    
+    //A function that counts down and then executes a function
+    func countDown(){
+        var timeLeft = 10
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            print("timer fired!")
+            timeLeft -= 1
+            print(timeLeft)
+                        
+            if(timeLeft==0){
+                timer.invalidate()
+            }
+        }
+    }
+    
+    
 }
 
